3ca465d76313c74f6482e8558b300b6d317089a3
Guilherme Augusto de Almeida Amaral <85243403+GuilhermeAAmaral@users.noreply.github.com>
2022-02-01 09:29:38 -0300

    atualização

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,6 +1,11 @@
-import { Box, Text, Image, SimpleGrid, VStack } from "@chakra-ui/react"
+import { Box, Text, Image, VStack, useBreakpointValue, Flex } from "@chakra-ui/react"
 
 export const Banner = () => {
+
+    const isWideVersion = useBreakpointValue({
+       base: false,
+       lg:true 
+    })
 
     return (
 
@@ -10,44 +15,47 @@ export const Banner = () => {
             bgRepeat="no-repeat"
             bgSize="cover"
             w="100%"
-            h="335px"
+            h={["183px","335px"]}
         >
 
-            <SimpleGrid columns={2} gap={5} alignItems="center">
-                
-                    <Box>
+            <Flex alignItems="center" gap={5} justifyContent="center">
+
+                <Box >
                     <VStack spacing={7} >
                         <Text
-                            fontSize="36px"
+                    
+                            fontSize={["25px", "36px"]}
                             fontWeight="medium"
                             color="gray.100"
-                            mr="50px"   
+                            mr={["70px","50px"]}
+                      
                         >
                             5 Continentes,<br/> infinitas possibilidades.
                         </Text>
-                        
+
                         <Text
-                            fontSize="18px"
+                    
+                            fontSize={["14px", "18px"]}
                             fontWeight="regular"
                             color="gray.300"
-                    
                         >
-                            Chegou a hora de tirar do papel a viagem que você<br/>sempre sonhou.
+                            Chegou a hora de tirar do papel a viagem que você<br />sempre sonhou.
                         </Text>
                     </VStack>
-                    </Box>
-               
-                    <Box>
-                        <Image
-                            src="images/Airplane.png"
-                            alt="foto do avião"
-                            position="relative"
-                            top="70px"
-                            left="80px"
-                        />
-                    </Box>
-                
-            </SimpleGrid>
+                </Box>
+
+                { isWideVersion &&                 
+                <Box>
+                    <Image
+                        src="images/Airplane.png"
+                        alt="foto do avião"
+                        position="relative"
+                        top="70px"
+                        left="80px"
+                    />
+                </Box>}
+
+            </Flex>
 
         </Box>
 
--- src/components/options/Options.tsx
+++ src/components/options/Options.tsx
@@ -1,49 +1,54 @@
-import { Flex, VStack, Box, Image, Text, HStack } from "@chakra-ui/react"
+import { Flex, VStack, Box, Image, Text, HStack, useBreakpointValue } from "@chakra-ui/react"
 
 export const Options = () => {
 
-    return (
+    const isWideVersion = useBreakpointValue({
+        base: false,
+        lg: true 
+    })
 
+    return (
+      
         <Flex
-            direction="row"
+            direction={["column", "row"]}
             algin="center"
             justify="center"
-            pt="80px"
+            mt="80px"
         >
 
-            <HStack spacing="145px">
+            <HStack spacing={["10px","145px"]}>
 
                 <Box>
                     <VStack spacing={4}>
-                        <Image src="images/Cocktail.png" />
+                        { isWideVersion && (<Image src="images/Cocktail.png" />)}
                         <Text fontSize="24px" fontWeight="semibold" color="gray.700">vida noturna</Text>
                     </VStack>
                 </Box>
 
                 <Box>
                     <VStack spacing={4}>
-                        <Image src="images/Beach.png" />
+                    { isWideVersion && (<Image src="images/Beach.png" />)}
                         <Text fontSize="24px" fontWeight="semibold" color="gray.700">praia</Text>
                     </VStack>
                 </Box>
 
                 <Box>
                     <VStack spacing={4}>
-                        <Image src="images/Modern.png" />
+                       {isWideVersion && (<Image src="images/Modern.png" />)} 
                         <Text fontSize="24px" fontWeight="semibold" color="gray.700">moderno</Text>
                     </VStack>
                 </Box>
 
                 <Box>
                     <VStack spacing={4}>
-                        <Image src="images/Classic.png" />
+                        {isWideVersion && (<Image src="images/Classic.png" />)}
                         <Text fontSize="24px" fontWeight="semibold" color="gray.700">clássico</Text>
                     </VStack>
                 </Box>
 
                 <Box>
                     <VStack spacing={3}>
-                        <Image src="images/More.png" />
+                        {isWideVersion && (<Image src="images/More.png" />)}
                         <Text fontSize="24px" fontWeight="semibold" color="gray.700">e mais...</Text>
                     </VStack>
                 </Box>
@@ -51,6 +56,7 @@ export const Options = () => {
             </HStack>
 
         </Flex>
+   
     )
 }
 
